7bfb46066577feecae2e4cb3b9f6756faa190d63
Christine <2023bcs004@std.must.ac.ug>
2025-12-19 12:06:06 +0300

    Add route constants for application navigation

---
+++ client/src/constants/routes.ts
@@ -0,0 +1,16 @@
+export const ROUTES = {
+  HOME: '/',
+  MERGE: '/merge',
+  SPLIT: '/split',
+  COMPRESS: '/compress',
+  ROTATE: '/rotate',
+  PROTECT: '/protect',
+  PDF_TO_JPG: '/pdf-to-jpg',
+  JPG_TO_PDF: '/jpg-to-pdf',
+  WORD_TO_PDF: '/word-to-pdf',
+  ABOUT: '/about',
+  PRIVACY: '/privacy',
+  TERMS: '/terms',
+  FAQ: '/faq',
+  CONTACT: '/contact'
+};
