30a59f90d5f7c7b219c9f6387f8efe84eaebc62a
Christine <2023bcs004@std.must.ac.ug>
2025-12-21 09:39:37 +0300

    Add array utility functions

---
+++ client/src/utils/array.ts
@@ -0,0 +1,9 @@
+export const chunk = <T>(array: T[], size: number): T[][] => {
+  return Array.from({ length: Math.ceil(array.length / size) }, (_, i) =>
+    array.slice(i * size, i * size + size)
+  );
+};
+
+export const unique = <T>(array: T[]): T[] => {
+  return [...new Set(array)];
+};
