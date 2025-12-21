04fd3b9ce2c36f284923887b9682e159dd6ddfc1
Christine <2023bcs004@std.must.ac.ug>
2025-12-21 09:41:02 +0300

    Add device detection utilities

---
+++ client/src/utils/device.ts
@@ -0,0 +1,7 @@
+export const isMobile = (): boolean => {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+};
+
+export const isTablet = (): boolean => {
+  return /iPad|Android/i.test(navigator.userAgent) && !isMobile();
+};
