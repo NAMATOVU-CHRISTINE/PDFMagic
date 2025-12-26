9c30b0bde2efd4f3161ab62e6fc837133d2e3cb7
Christine <2023bcs004@std.must.ac.ug>
2025-12-26 19:05:24 +0300

    Add hooks barrel export file

---
+++ client/src/hooks/index.ts
@@ -0,0 +1,15 @@
+// Custom Hooks
+export { useDebounce, useDebouncedCallback } from './useDebounce';
+export { useFocusTrap } from './useFocusTrap';
+export { useKeyboardShortcuts, SHORTCUTS } from './useKeyboardShortcuts';
+export { useLocalStorage } from './useLocalStorage';
+export {
+  useMediaQuery,
+  useIsMobile,
+  useIsTablet,
+  useIsDesktop,
+  usePrefersDarkMode,
+  usePrefersReducedMotion,
+} from './useMediaQuery';
+export { useRecentFiles } from './useRecentFiles';
+export { useToast } from './useToast';
