066ecbf4370f1e07ae0f0d8733561dead607ba2f
Christine <2023bcs004@std.must.ac.ug>
2025-12-27 10:36:12 +0300

    Add throttle utility function

---
+++ client/src/utils/throttle.ts
@@ -0,0 +1,16 @@
+export function throttle<T extends (...args: unknown[]) => unknown>(
+  func: T,
+  limit: number
+): (...args: Parameters<T>) => void {
+  let inThrottle = false;
+
+  return function (...args: Parameters<T>) {
+    if (!inThrottle) {
+      func(...args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
+
+export default throttle;
