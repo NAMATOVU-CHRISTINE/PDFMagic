5752ec9788182a823485cc40deeb5773fae36e35
Christine <2023bcs004@std.must.ac.ug>
2025-12-27 10:35:07 +0300

    Add debounce utility function

---
+++ client/src/utils/debounce.ts
@@ -0,0 +1,13 @@
+export function debounce<T extends (...args: unknown[]) => unknown>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeoutId: ReturnType<typeof setTimeout> | null = null;
+
+  return function (...args: Parameters<T>) {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func(...args), wait);
+  };
+}
+
+export default debounce;
