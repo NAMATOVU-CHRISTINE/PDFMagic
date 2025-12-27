445ccbf4e242e3d828d51d9da698682eb84880d9
Christine <2023bcs004@std.must.ac.ug>
2025-12-27 10:11:58 +0300

    Add Divider component

--- client/src/components/Divider.tsx
+++ client/src/components/Divider.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
 
 interface DividerProps {
-  orientation?: 'horizontal' | 'vertical';
+  darkMode?: boolean;
   className?: string;
-  text?: string;
+  vertical?: boolean;
 }
 
-const Divider: React.FC<DividerProps> = ({ orientation = 'horizontal', className = '', text }) => {
-  if (orientation === 'vertical') {
-    return <div className={`w-px bg-gray-200 ${className}`} />;
+const Divider: React.FC<DividerProps> = ({ 
+  darkMode = false, 
+  className = '',
+  vertical = false 
+}) => {
+  const colorClass = darkMode ? 'bg-gray-700' : 'bg-gray-200';
+  
+  if (vertical) {
+    return <div className={`w-px h-full ${colorClass} ${className}`} />;
   }
 
-  if (text) {
-    return (
-      <div className={`relative flex items-center ${className}`}>
-        <div className="flex-grow border-t border-gray-200" />
-        <span className="px-4 text-sm text-gray-500">{text}</span>
-        <div className="flex-grow border-t border-gray-200" />
-      </div>
-    );
-  }
-
-  return <div className={`border-t border-gray-200 ${className}`} />;
+  return <hr className={`border-0 h-px ${colorClass} ${className}`} />;
 };
 
 export default Divider;
