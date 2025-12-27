f78724873fa74cdbf9e2e307b5c69470eda0b911
Christine <2023bcs004@std.must.ac.ug>
2025-12-27 10:36:19 +0300

    Add error handler utility

---
+++ server/src/utils/errorHandler.js
@@ -0,0 +1,22 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+const handleError = (err, res) => {
+  const { statusCode = 500, message } = err;
+  
+  res.status(statusCode).json({
+    status: 'error',
+    statusCode,
+    message: statusCode === 500 ? 'Internal server error' : message,
+  });
+};
+
+module.exports = { AppError, handleError };
