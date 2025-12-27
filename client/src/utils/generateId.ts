dc167747314ce8e86ace375ef27de3131334a3d3
Christine <2023bcs004@std.must.ac.ug>
2025-12-27 10:37:25 +0300

    Add ID generation utilities

---
+++ client/src/utils/generateId.ts
@@ -0,0 +1,15 @@
+export function generateId(prefix = ''): string {
+  const timestamp = Date.now().toString(36);
+  const random = Math.random().toString(36).substring(2, 9);
+  return prefix ? `${prefix}_${timestamp}${random}` : `${timestamp}${random}`;
+}
+
+export function generateUUID(): string {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+export default generateId;
