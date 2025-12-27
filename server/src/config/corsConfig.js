410a6cfa340c66f2987173a8aaf61e7d3ab74f22
Christine <2023bcs004@std.must.ac.ug>
2025-12-27 10:06:49 +0300

    Add CORS configuration module

---
+++ server/src/config/corsConfig.js
@@ -0,0 +1,9 @@
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN || '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  maxAge: 86400, // 24 hours
+};
+
+export default corsOptions;
