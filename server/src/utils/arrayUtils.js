31c665422405164183717eb93a79261ce0eaf1d6
Christine <2023bcs004@std.must.ac.ug>
2025-12-29 22:43:29 +0300

    Add array utility functions

---
+++ server/src/utils/arrayUtils.js
@@ -0,0 +1,20 @@
+const chunk = (array, size) => {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += size) {
+    chunks.push(array.slice(i, i + size));
+  }
+  return chunks;
+};
+
+const shuffle = (array) => {
+  const result = [...array];
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+  return result;
+};
+
+const unique = (array) => [...new Set(array)];
+
+module.exports = { chunk, shuffle, unique };
