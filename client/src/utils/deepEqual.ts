c6a8e632e145b4491740db122b40dabe0a63cdff
Christine <2023bcs004@std.must.ac.ug>
2025-12-29 22:46:32 +0300

    Add deepEqual utility function

---
+++ client/src/utils/deepEqual.ts
@@ -0,0 +1,17 @@
+export function deepEqual(a: unknown, b: unknown): boolean {
+  if (a === b) return true;
+  if (typeof a !== typeof b) return false;
+  if (a === null || b === null) return a === b;
+  if (typeof a !== 'object') return false;
+
+  const keysA = Object.keys(a as object);
+  const keysB = Object.keys(b as object);
+
+  if (keysA.length !== keysB.length) return false;
+
+  return keysA.every(key => 
+    deepEqual((a as Record<string, unknown>)[key], (b as Record<string, unknown>)[key])
+  );
+}
+
+export default deepEqual;
