efee981cb18865f9d3e9b6fbcbd3f19dce2948ce
Christine <2023bcs004@std.must.ac.ug>
2025-12-29 22:43:19 +0300

    Add useUpdateEffect hook

---
+++ client/src/hooks/useUpdateEffect.ts
@@ -0,0 +1,15 @@
+import { useEffect, useRef, DependencyList, EffectCallback } from 'react';
+
+export function useUpdateEffect(effect: EffectCallback, deps?: DependencyList): void {
+  const isFirstMount = useRef(true);
+
+  useEffect(() => {
+    if (isFirstMount.current) {
+      isFirstMount.current = false;
+      return;
+    }
+    return effect();
+  }, deps);
+}
+
+export default useUpdateEffect;
