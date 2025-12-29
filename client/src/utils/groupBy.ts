bf78d1a5c83c780575a553eca5d834fbd17dc41f
Christine <2023bcs004@std.must.ac.ug>
2025-12-29 22:43:53 +0300

    Add groupBy utility function

---
+++ client/src/utils/groupBy.ts
@@ -0,0 +1,12 @@
+export function groupBy<T>(array: T[], key: keyof T): Record<string, T[]> {
+  return array.reduce((result, item) => {
+    const groupKey = String(item[key]);
+    if (!result[groupKey]) {
+      result[groupKey] = [];
+    }
+    result[groupKey].push(item);
+    return result;
+  }, {} as Record<string, T[]>);
+}
+
+export default groupBy;
