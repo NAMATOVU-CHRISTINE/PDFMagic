5d873bfb09c28ce9d263e5ae44c6facf924d269c
Christine <2023bcs004@std.must.ac.ug>
2025-12-29 22:51:01 +0300

    Add useRenderCount hook

---
+++ client/src/hooks/useRenderCount.ts
@@ -0,0 +1,9 @@
+import { useRef } from 'react';
+
+export function useRenderCount(): number {
+  const count = useRef(0);
+  count.current += 1;
+  return count.current;
+}
+
+export default useRenderCount;
