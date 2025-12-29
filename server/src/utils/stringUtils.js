6b7186fdc0cc6beb486be8e92f173aa9da59451e
Christine <2023bcs004@std.must.ac.ug>
2025-12-29 22:40:53 +0300

    Add string utility functions for server

---
+++ server/src/utils/stringUtils.js
@@ -0,0 +1,19 @@
+const slugify = (text) => {
+  return text
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, '')
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '');
+};
+
+const generateRandomString = (length = 10) => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+};
+
+module.exports = { slugify, generateRandomString };
