44fee3884ae4624b1aed7a07f7a008afd29f216e
Christine <2023bcs004@std.must.ac.ug>
2025-12-29 22:51:18 +0300

    Add useLatest hook

---
+++ client/src/hooks/useLatest.ts
@@ -0,0 +1,13 @@
+import { useRef, useEffect } from 'react';
+
+export function useLatest<T>(value: T): { readonly current: T } {
+  const ref = useRef(value);
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref;
+}
+
+export default useLatest;
