dcee9a86b16f6f66482ec3318912ab640d815ed8
Christine <2023bcs004@std.must.ac.ug>
2025-12-30 15:06:51 +0300

    Add arrayMove utility

---
+++ client/src/utils/arrayMove.ts
@@ -0,0 +1,8 @@
+export function arrayMove<T>(arr: T[], from: number, to: number): T[] {
+  const result = [...arr];
+  const [item] = result.splice(from, 1);
+  result.splice(to, 0, item);
+  return result;
+}
+
+export default arrayMove;
