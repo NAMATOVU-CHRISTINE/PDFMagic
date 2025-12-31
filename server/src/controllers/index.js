592fe8502da4f7e9fceb469bf88a1e1284e3750d
Christine <2023bcs004@std.must.ac.ug>
2025-12-31 09:25:24 +0300

    Add controllers barrel export

---
+++ server/src/controllers/index.js
@@ -0,0 +1,15 @@
+const pdfController = require('./pdfController');
+const convertController = require('./convertController');
+const rotateController = require('./rotateController');
+const watermarkController = require('./watermarkController');
+const protectController = require('./protectController');
+const pageController = require('./pageController');
+
+module.exports = {
+  pdfController,
+  convertController,
+  rotateController,
+  watermarkController,
+  protectController,
+  pageController,
+};
