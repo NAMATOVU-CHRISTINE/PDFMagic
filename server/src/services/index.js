7b10c65704ef884b1c8993a1f1a5196c09866462
Christine <2023bcs004@std.must.ac.ug>
2025-12-31 09:25:52 +0300

    Add services barrel export

---
+++ server/src/services/index.js
@@ -0,0 +1,15 @@
+const pdfService = require('./pdfService');
+const convertService = require('./convertService');
+const rotateService = require('./rotateService');
+const watermarkService = require('./watermarkService');
+const protectService = require('./protectService');
+const pageService = require('./pageService');
+
+module.exports = {
+  pdfService,
+  convertService,
+  rotateService,
+  watermarkService,
+  protectService,
+  pageService,
+};
