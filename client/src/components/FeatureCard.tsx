2943f15d90430814606689396e1d682405730d12
Christine <2023bcs004@std.must.ac.ug>
2026-01-01 19:05:24 +0300

    Add FeatureCard component

---
+++ client/src/components/FeatureCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface FeatureCardProps {
+  icon: React.ReactNode;
+  title: string;
+  description: string;
+}
+
+export const FeatureCard: React.FC<FeatureCardProps> = ({ icon, title, description }) => {
+  return (
+    <div className="p-6 bg-white rounded-lg shadow-md text-center">
+      <div className="text-3xl mb-4">{icon}</div>
+      <h3 className="font-semibold text-lg mb-2">{title}</h3>
+      <p className="text-gray-600">{description}</p>
+    </div>
+  );
+};
+
+export default FeatureCard;
