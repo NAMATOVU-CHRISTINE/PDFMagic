94dff8a065e5c7c35691aaa10d9d079efc21c7cf
Christine <2023bcs004@std.must.ac.ug>
2026-01-01 19:07:54 +0300

    Add OCR service

---
+++ server/src/services/ocrService.js
@@ -0,0 +1,8 @@
+const fs = require('fs').promises;
+
+const extractText = async (file) => {
+  const text = 'OCR text extraction placeholder';
+  return { text };
+};
+
+module.exports = { extractText };
