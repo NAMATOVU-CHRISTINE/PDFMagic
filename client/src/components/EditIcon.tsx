755c3649918c08b677fd8844151d0e2bf468b645
Christine <2023bcs004@std.must.ac.ug>
2026-01-03 00:01:33 +0300

    Add EditIcon component

---
+++ client/src/components/EditIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface EditIconProps {
+  size?: number;
+}
+
+export const EditIcon: React.FC<EditIconProps> = ({ size = 24 }) => {
+  return (
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+      <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+      <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+    </svg>
+  );
+};
+
+export default EditIcon;
