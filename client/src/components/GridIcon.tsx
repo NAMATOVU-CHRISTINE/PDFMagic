bba8dcd0bf1c6deae8456bbc819fc73fd2e2fb04
Christine <2023bcs004@std.must.ac.ug>
2026-01-03 19:58:41 +0300

    Add GridIcon component

---
+++ client/src/components/GridIcon.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface GridIconProps {
+  size?: number;
+}
+
+export const GridIcon: React.FC<GridIconProps> = ({ size = 24 }) => {
+  return (
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+      <rect x="3" y="3" width="7" height="7" />
+      <rect x="14" y="3" width="7" height="7" />
+      <rect x="14" y="14" width="7" height="7" />
+      <rect x="3" y="14" width="7" height="7" />
+    </svg>
+  );
+};
+
+export default GridIcon;
