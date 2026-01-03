d875316fd48145b75c88e4e65c0e6546c8c727d0
Christine <2023bcs004@std.must.ac.ug>
2026-01-03 20:05:17 +0300

    Add CompareIcon component

---
+++ client/src/components/CompareIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface CompareIconProps {
+  size?: number;
+}
+
+export const CompareIcon: React.FC<CompareIconProps> = ({ size = 24 }) => {
+  return (
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+      <rect x="3" y="3" width="8" height="18" rx="1" />
+      <rect x="13" y="3" width="8" height="18" rx="1" />
+    </svg>
+  );
+};
+
+export default CompareIcon;
