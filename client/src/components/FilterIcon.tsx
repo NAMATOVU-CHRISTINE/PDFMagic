287ae8ea109d65256d2ce45efbc80c6e955b2ebe
Christine <2023bcs004@std.must.ac.ug>
2026-01-03 20:01:24 +0300

    Add FilterIcon component

---
+++ client/src/components/FilterIcon.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface FilterIconProps {
+  size?: number;
+}
+
+export const FilterIcon: React.FC<FilterIconProps> = ({ size = 24 }) => {
+  return (
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+      <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3" />
+    </svg>
+  );
+};
+
+export default FilterIcon;
