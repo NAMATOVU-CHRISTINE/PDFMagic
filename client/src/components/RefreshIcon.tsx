04a90d3f9dc32310aa0e7119bb06f0d436476012
Christine <2023bcs004@std.must.ac.ug>
2026-01-03 20:11:58 +0300

    Add RefreshIcon component

---
+++ client/src/components/RefreshIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface RefreshIconProps {
+  size?: number;
+}
+
+export const RefreshIcon: React.FC<RefreshIconProps> = ({ size = 24 }) => {
+  return (
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+      <polyline points="23 4 23 10 17 10" />
+      <path d="M20.49 15a9 9 0 1 1-2.12-9.36L23 10" />
+    </svg>
+  );
+};
+
+export default RefreshIcon;
