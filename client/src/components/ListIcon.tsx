a3a938d6363684d66ad53933ce06ae95e98970f5
Christine <2023bcs004@std.must.ac.ug>
2026-01-03 20:00:36 +0300

    Add ListIcon component

---
+++ client/src/components/ListIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface ListIconProps {
+  size?: number;
+}
+
+export const ListIcon: React.FC<ListIconProps> = ({ size = 24 }) => {
+  return (
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+      <line x1="8" y1="6" x2="21" y2="6" />
+      <line x1="8" y1="12" x2="21" y2="12" />
+      <line x1="8" y1="18" x2="21" y2="18" />
+      <line x1="3" y1="6" x2="3.01" y2="6" />
+      <line x1="3" y1="12" x2="3.01" y2="12" />
+      <line x1="3" y1="18" x2="3.01" y2="18" />
+    </svg>
+  );
+};
+
+export default ListIcon;
