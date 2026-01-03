d2507fe738ec202625b2d8199c3943b69e8193c6
Christine <2023bcs004@std.must.ac.ug>
2026-01-03 20:09:50 +0300

    Add HomeIcon component

---
+++ client/src/components/HomeIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface HomeIconProps {
+  size?: number;
+}
+
+export const HomeIcon: React.FC<HomeIconProps> = ({ size = 24 }) => {
+  return (
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+      <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+      <polyline points="9 22 9 12 15 12 15 22" />
+    </svg>
+  );
+};
+
+export default HomeIcon;
