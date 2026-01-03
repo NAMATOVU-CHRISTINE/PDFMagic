e5971d002e4ce4399a070c5a5181ffea49d4d401
Christine <2023bcs004@std.must.ac.ug>
2026-01-03 20:08:05 +0300

    Add DeleteIcon component

---
+++ client/src/components/DeleteIcon.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface DeleteIconProps {
+  size?: number;
+}
+
+export const DeleteIcon: React.FC<DeleteIconProps> = ({ size = 24 }) => {
+  return (
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+      <path d="M21 4H8l-7 8 7 8h13a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z" />
+      <line x1="18" y1="9" x2="12" y2="15" />
+      <line x1="12" y1="9" x2="18" y2="15" />
+    </svg>
+  );
+};
+
+export default DeleteIcon;
